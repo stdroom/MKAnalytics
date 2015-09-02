86eb0d4597b1a57f14ac190e6ceafdd6f8a7d43c
leixun <leixun@xiaoma.cn>
2015-09-02 09:50:59 +0800

    特别关注

---
+++ sdk/SpecialFocus/src/com/sepcialfocus/android/BaseApplication.java
@@ -0,0 +1,25 @@
+/**
+ * 工程名: SpecialFocus
+ * 文件名: BaseApplication.java
+ * 包名: com.sepcialfocus.android
+ * 日期: 2015-9-1下午8:47:25
+ * Copyright (c) 2015, 北京小马过河教育科技有限公司 All Rights Reserved.
+ * http://www.xiaoma.com/
+ * Mail: leixun@xiaoma.cn
+ * QQ: 378640336
+ *
+*/
+
+package com.sepcialfocus.android;
+/**
+ * 类名: BaseApplication <br/>
+ * 功能: TODO 添加功能描述. <br/>
+ * 日期: 2015-9-1 下午8:47:25 <br/>
+ *
+ * @author   leixun
+ * @version  	 
+ */
+public class BaseApplication {
+
+}
+
